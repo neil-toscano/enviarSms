dc014c9caac22555431145c43e849bb25b462255
neil <jacoborosseau@gmail.com>
2023-03-31 11:29:10 -0500

    enviarsms

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const accountSid = 'AC8439b9664711403479da53efb7e92755';
-const authToken = '399b928916eb8aa19979898f02470767';
+const authToken = '02684e0b26cc611257e7c8a900dc9854';
 const client = require('twilio')(accountSid, authToken);
 const express = require('express');
 const app = express();
@@ -11,12 +11,13 @@ const port=process.env.PORT;
   app.use(cors());
   
 app.get('/enviarsms', function (req, res) {
+  let mensaje=req.query.mensaje;
     res.send("hello world");
     client.messages
   .create({
-     body: 'Robotica gaaaaaaaaa!',
+     body: mensaje,
      from: '+14346026558',
-     to: '+5191816630'
+     to: '+51946684130'
    })
   .then(message => console.log(`Mensaje enviado: ${message.sid}`));
   
